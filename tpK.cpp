25508e67d1841dbd3b12d0f7649d38925ba05442
DaNastya <61517785+DaNastya@users.noreply.github.com>
2020-12-24 20:01:10 +0300

    Update tpK.cpp

--- tpK.cpp
+++ tpK.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include"kripto.h"
 using namespace std;
 int main()
@@ -7,9 +6,7 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	vector<cripto*> C;
-	Ceasar Ces;
-	Vishner Vis;
-	Enigma Eni;
+	Ceasar Ces;Vishner Vis;Enigma Eni;
 	string namef;
 	int num = 1;
 	bool inf = false;
@@ -17,12 +14,12 @@ int main()
 	ifstream fin;
 	fstream fout;
 	int i = 0;
-	while (num != 0)
+	while (true)
 	{
 		system("cls");
-		cout << "Выберите входной поток данных:" << endl;
-		cout << "|1| - Консоль;" << endl;
-		cout << "|2| - Файл;" << endl;
+		cout << "Выберите откуда будет производиться запись:" << endl;
+		cout << "|1| - С консоли" << endl;
+		cout << "|2| - Из файла" << endl;
 		cin >> num;
 		try {
 			if (num == 2) {
@@ -35,29 +32,11 @@ int main()
 				if (!fin.is_open())
 					throw "\x1b[31mНевозможно найти файл с таким именем\x1b[0m";
 			}
-			cout << "Выберите выходной поток данных:" << endl;
-			cout << "|1| - Консоль;" << endl;
-			cout << "|2| - Файл;" << endl;
-			cin >> num;
-			if (num == 2)
-			{
-				off = true;
-				cout << "Введите название файла(без формата): " << endl;
-				cin.get();
-				getline(cin, namef);
-				namef = initName(namef);
-				if (!formatname(namef)) throw namef;
-				fout.open(namef, ios::out);
-			}
 		}
 		catch (const char* error) {
 			cout << error << endl;
 			system("pause");
 			continue;
-		}
-		catch (string namef) {
-			cout << "\x1b[31mНельзя создать файл с таким именем\x1b[0m\nИмя файла измененено на " << namef << endl;
-			fout.open(namef, ios::out);
 		}
 		system("pause");
 		system("cls");
@@ -67,35 +46,21 @@ int main()
 		if (num == 1) {
 			cout << "|1|-Шифр Цезаря\n|2|-Шифр Вижнера\n|3|-Шифр Энигма\n|0|- для выхода\n->";
 			cin >> num;
-
 			switch (num)
 			{
 			case 1:
 				C.push_back(new Ceasar());
-				C[i]->cod(
-					(inf == true) ? fin : cin,
-					(off == true) ? fout : cout,
-					(inf == true) ? true : false,
-					(off == true) ? true : false
-				);
+				C[i]->cod((inf == true) ? fin : cin,inf,off );
 				i++;
 				break;
 			case 2:
 				C.push_back(new Vishner());
-				C[i]->cod(
-					(inf == true) ? fin : cin,
-					(off == true) ? fout : cout,
-					(inf == true) ? true : false,
-					(off == true) ? true : false);
+				C[i]->cod((inf == true) ? fin : cin,inf ,off );
 				i++;
 				break;
 			case 3:
 				C.push_back(new Enigma());
-				C[i]->cod(
-					(inf == true) ? fin : cin,
-					(off == true) ? fout : cout,
-					(inf == true) ? true : false,
-					(off == true) ? true : false);
+				C[i]->cod((inf == true) ? fin : cin,inf,off);
 				i++;
 				break;
 
@@ -106,22 +71,28 @@ int main()
 			system("pause");
 		}
 		else if (num == 2) {
-
-			try {
+			
 				if (inf == false) {
-					if (C.size() == 0) throw "\x1b[31mВы не кодировали сообщения, выберите файл или закодируйте какое-нибудь сообщение\x1b[0m";
-					cout << "Выберите сообщение для декодирования" << endl;
-					for (size_t i = 0; i < C.size(); i++) {
-						cout << i + 1 << " " << C[i]->getlen() << endl;
+					while (true) {
+						try {
+							if (C.size() == 0) throw "\x1b[31mВы не кодировали сообщения, выберите файл или закодируйте какое-нибудь сообщение\x1b[0m";
+							cout << "Выберите сообщение для декодирования" << endl;
+							for (size_t i = 0; i < C.size(); i++) {
+								cout << i + 1 << "|" << endl;
+								C[i]->getlen();
 
+							}
+							cin >> i;
+							if (i > C.size()) throw i;
+							else break;
+						}
+						catch (int i) {
+							cout << "Ведено не верное значение, выберете сообщение под номером меньше " << C.size() << endl;
+							cin >> i;
+						
+						}
 					}
-					cin >> i;
-					C[i - 1]->decod(
-						cin,
-						(off == true) ? fout : cout,
-						false,
-						(off == true) ? true : false
-					);
+					C[i - 1]->decod(cin,false,off);
 					system("pause");
 				}
 				else {
@@ -132,47 +103,27 @@ int main()
 					{
 					case 1:
 						C.push_back(new Ceasar());
-						C[i]->cod(
-							fin,
-							(off == true) ? fout : cout,
-							true,
-							(off == true) ? true : false
-						);
+						C[i]->cod(fin,true,off);
 						break;
 					case 2:
 						C.push_back(new Vishner());
-						C[i]->cod(
-							fin,
-							(off == true) ? fout : cout,
-							true,
-							(off == true) ? true : false);
+						C[i]->cod(fin,true,off);
 
 						break;
 					case 3:
 						C.push_back(new Enigma());
-						C[i]->cod(
-							fin,
-							(off == true) ? fout : cout,
-							false,
-							(off == true) ? true : false);
+						C[i]->cod(fin,true,off);
 
 						break;
 
 					default:
 						cout << "Выход в меню" << endl;
 						break;
-					
-						
 					}
 					system("pause");
 
 				}
 			}
-			catch (const char* error) {
-				cout << error << endl;
-			}
-
-		}
 		else if (num == 0) {
 			C.clear();
 			if (inf == true)fin.close();
@@ -180,8 +131,32 @@ int main()
 			inf = off = false;
 			return 0;
 		}
+		cout << "Хотите записать сообщение в файл?" << endl;
+		cout << "|1| - Да" << endl;
+		cout << "|2| - Нет" << endl;
+		cin >> num;
+		if (num == 1)
+		{
+			try {
+				off = true;
+				cout << "Введите название файла(без формата): " << endl;
+				cin.get();
+				getline(cin, namef);
+				namef = initName(namef);
+				if (!formatname(namef)) throw namef;
+
+			}
+			catch (string namef) {
+				cout << "\x1b[31mНельзя создать файл с таким именем\x1b[0m\nИмя файла измененено на " << namef << endl;
+				
+			}
+			fout.open(namef, ios::out);
+			C[i - 1]->recording(fout);
+			fout.close();
+			system("pause");
+		}
+			
 		if (inf == true)fin.close();
-		if (off == true)fout.close();
 		inf = off = false;
 	}
 
